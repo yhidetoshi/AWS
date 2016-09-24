ab18b9e457759fcd157f2da9cc307da4764ee8ca
yhidetoshi <hyajima.1014@gmail.com>
2016-09-24 10:12:23 +0900

    add msg-fcuntion

--- Script-For-Jenkins/create-instance.sh
+++ Script-For-Jenkins/create-instance.sh
@@ -1,1 +1,36 @@
-aws ec2 run-instances --image-id ami-374db956 --count 1 --region ap-northeast-1 --instance-type t2.nano
+#!/bin/bash
+
+RETRY_LIMIT=10
+RETRY_COUNT=0
+RETRY_SLEEP=2
+
+function msg(){
+  msg_=$1
+  case $msg_ in
+    success)
+       echo "success!"
+       ;;
+    failed)
+       echo "failed!"
+       ;;
+  esac
+}
+
+while :
+  do
+       aws ec2 run-instances --image-id ami-374db956 --count 1 --region ap-northeast-1 --instance-type t2.nano
+       echo \n
+    if [ $? -eq 0 ]; then
+         msg success
+         break
+
+    elif [ ${RETRY_COUNT} -lt ${RETRY_LIMIT} ]; then
+          RETRY_COUNT=`expr ${RETRY_COUNT} + 1`
+          echo ${RETRY_LIMIT}
+          echo ${RETRY_COUNT}
+          sleep ${RETRY_SLEEP};
+    else
+          msg failed
+          break
+    fi
+  done
