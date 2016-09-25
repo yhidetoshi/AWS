64306613d1f2eeaacad7779670ff22d95182ee80
Hidetoshi Yajima <hyajima.jaist.job@gmail.com>
2016-09-25 15:06:08 +0900

    Register Route53 to A_record using userdata

---
+++ User-Data/Regist_Route53_A_record.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+
+RETRY_LIMIT=10
+RETRY_COUNT=0
+RETRY_SLEEP=2
+DOMAIN="dev.yajima"
+
+# yum install jq
+
+
+function msg(){
+  msg_=$1
+  case $msg_ in
+    success)
+      echo "success!"
+      ;;
+    failed)
+      echo "failed!"
+      ;;
+  esac
+}
+
+# Get-Private-IP
+IPV4=`/usr/bin/curl -s http://169.254.169.254/latest/meta-data/local-ipv4`
+
+# Get-Hostname
+HOSTNAME=`/usr/bin/curl -s http://169.254.169.254/latest/meta-data/hostname`
+SHORT_HOSTNAME=`expr ${HOSTNAME} | sed -e 's/ap-northeast-1.compute.internal//i'`
+
+
+
+# Get-Hosted-Zone-ID
+while :
+ do
+     HOSTED_ZONE_ID=`aws route53 list-hosted-zones | jq --arg zone_name ${DOMAIN} -r '.HostedZones[]|select(.Name |contains($zone_name))|.Id' | sed 's/\/hostedzone\///'`
+
+   if [ $? -eq 0 ]; then
+       echo ${HOSTED_ZONE_ID}
+       msg success
+       break
+
+   elif [ ${RETRY_COUNT} -lt ${RETRY_LIMIT} ]; then
+       RETRY_COUNT=`expr ${RETRY_COUNT} + 1`
+       sleep ${RETRY_SLEEP};
+   else
+      msg failed
+       break
+   fi
+ done
+
+# Output Jsonfile for A-record
+cat <<EOF > /tmp/Arecord-set.json
+{
+     "Changes": [
+      {
+        "Action": "CREATE",
+        "ResourceRecordSet": {
+           "Name": "${SHORT_HOSTNAME}${DOMAIN}",
+           "ResourceRecords": [
+              {
+                  "Value": "${IPV4}"
+              }
+           ],
+           "Type": "A",
+           "TTL": 60
+           }
+         }
+       ]
+}
+EOF
+cat /tmp/Arecord-set.json
+
+# Route53にAレコードを追加する
+while :
+ do
+     aws route53 change-resource-record-sets --hosted-zone-id ${HOSTED_ZONE_ID} --change-batch file:///tmp/Arecord-set.json
+
+   if [ $? -eq 0 ]; then
+       msg success
+       break
+
+   elif [ ${RETRY_COUNT} -lt ${RETRY_LIMIT} ]; then
+       RETRY_COUNT=`expr ${RETRY_COUNT} + 1`
+       sleep ${RETRY_SLEEP};
+   else
+      msg failed
+       break
+   fi
+ done
