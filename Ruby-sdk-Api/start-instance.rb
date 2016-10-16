6656dd5f18ae4ecfb231f7a5f341b170550e0c9d
yhidetoshi <hyajima.1014@gmail.com>
2016-10-16 15:51:57 +0900

    update

--- Ruby-sdk-Api/start-instance.rb
+++ Ruby-sdk-Api/start-instance.rb
@@ -1,10 +1,11 @@
 require 'aws-sdk-core'
 
   ec2 = Aws::EC2::Client.new
+  instanceID = 'i-06a861ed7a1685e8f'
 
   begin
-    ec2.start_instances(instance_ids: ['i-df869950'])
-    ec2.wait_until(:instance_running, instance_ids:['i-df869950'])
+    ec2.start_instances(instance_ids: [instanceID])
+    ec2.wait_until(:instance_running, instance_ids:[instanceID])
     puts "Instance is Running"
   rescue Aws::Waiters::Errors::WaiterFailed => error
     puts "failed waiting for Instance Running: #{error.message}"
