f3d5938134df1c47ac3bca9bb2c8b7a0903c299a
Hidetoshi Yajima <hyajima.jaist.job@gmail.com>
2016-12-09 12:38:44 +0900

    Update Regist_Route53_A_record.sh

--- User-Data/Regist_Route53_A_record.sh
+++ User-Data/Regist_Route53_A_record.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+#!/bin/bash
+mkdir /root/.aws
+echo '[default]' >> /root/.aws/config
+echo 'output = json' >> /root/.aws/config
+echo 'region = ap-northeast-1' >> /root/.aws/config
+echo '[default]' >> /root/.aws/credentials
+echo 'aws_access_key_id = <aws_access_key_id>' >> /root/.aws/credentials
+echo 'aws_secret_access_key = <aws_secret_access_key>' >> /root/.aws/credentials
+
 
 RETRY_LIMIT=10
 RETRY_COUNT=0
